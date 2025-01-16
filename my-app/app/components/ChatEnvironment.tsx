8d7ab7d799e005c54df39f7fbb5979a36fa9f737
fmasz6 <fmasz6@gmail.com>
2025-01-15 22:10:21 -0400

    Updated chat input design with modern styling

--- my-app/app/components/ChatEnvironment.tsx
+++ my-app/app/components/ChatEnvironment.tsx
@@ -40,14 +40,55 @@ const ChatEnvironment = () => {
               </div>
             ))}
           </div>
-          <div className="mt-2 flex">
-            <input
-              type="text"
-              className="flex-1 border rounded p-2 mr-2"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-            <button className="bg-blue-500 text-white rounded p-2" onClick={handleSendMessage}>Send</button>
+          <div className="mt-2 relative">
+            <div className="flex items-center rounded-full border p-1 pl-12 pr-16">
+              <button 
+                className="absolute left-3 p-2 text-gray-500 hover:text-gray-700"
+                onClick={() => document.getElementById('file-input')?.click()}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
+                </svg>
+                <input 
+                  type="file" 
+                  id="file-input" 
+                  className="hidden"
+                  onChange={(e) => {
+                    const file = e.target.files?.[0];
+                    if (file) {
+                      // Handle file upload
+                    }
+                  }}
+                />
+              </button>
+              <input
+                type="text"
+                className="flex-1 bg-transparent outline-none px-2"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Type your message..."
+              />
+              <div className="absolute right-3 flex space-x-2">
+                <button 
+                  className="p-2 text-gray-500 hover:text-gray-700"
+                  onClick={handleSendMessage}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+                  </svg>
+                </button>
+                <button 
+                  className="p-2 text-gray-500 hover:text-gray-700"
+                  onClick={() => {
+                    // Handle microphone click
+                  }}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clipRule="evenodd" />
+                  </svg>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
